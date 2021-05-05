098fb9a98257513166530ad2283049a799044d54
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2021-05-05 09:54:21 -0400

    adding source code and license

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # rpi_hardware_pwm
-Access the hardware PWM of a RaspberryPi
+
+Access the hardware PWM of a RaspberryPi with Python. More lightweight than alternatives.
+
+### History
+
+The original code is from (jdimpson/syspwm)[https://github.com/jdimpson/syspwm], I've updated it to Python3 and
+made it look like the `RPi.GPIO` library's API (but more Pythonic than that.)
+
---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+from setuptools import find_packages
+import os
+
+
+# User-friendly description from README.md
+current_directory = os.path.dirname(os.path.abspath(__file__))
+try:
+    with open(os.path.join(current_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+except Exception:
+    long_description = ''
+
+setup(
+    name="rpi_hardware_pwm",
+    packages=find_packages('.'),
+    version='0.0.1',
+    license='OSI Approved :: GNU General Public License v3 (GPLv3)',
+    description='Control Hardware PWM on the Raspberry Pi',
+    long_description = long_description,
+    long_description_context_type = 'text/markdown',
+    author='Cam Davidson-Pilon',
+    author_email='cam.davidson.pilon@gmail.com',
+    url='https://github.com/Pioreactor/rpi_hardware_pwm',
+    keywords=["raspberry pi", "pwm"],
+    install_requires=[],
+    python_requires='>=3',
+    # https://pypi.org/classifiers/
+    classifiers=["Development Status :: 4 - Beta", "Topic :: System :: Hardware", ]
+)
